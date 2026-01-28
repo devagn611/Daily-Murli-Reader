0e1f569a14bf545eb65910c70c3cf57658aba9c1
devagn611 <devagnmaniya611@gmail.com>
2026-01-28 14:03:26 +0530

    added_caching

--- backend/server.ts
+++ backend/server.ts
@@ -16,6 +16,7 @@ const allowedOrigins = process.env.NODE_ENV === 'production'
       'https://murli.devagn.com',
       'http://localhost:5173',
       'http://localhost:3000',
+      "https://daily-murli-reader-fron.vercel.app"
     ])
   : ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:5173', process.env.FRONTEND_URL].filter((url): url is string => Boolean(url));
 
